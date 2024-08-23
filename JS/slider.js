01a9c6599bc432cf7d01d2111d2e7e187df3d600
JuSally <juliapetersenavi@gmail.com>
2024-08-23 16:50:56 -0300

    Add files via upload

---
+++ JS/slider.js
@@ -0,0 +1,28 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 15,
+    centeredSlides: false,
+    loop: true,
+    slidesPerGroupSkip: 1,
+    grabCursor: true,
+    keyboard: {
+        enabled: true,
+    },
+    breakpoints: {
+        769: {
+            slidesPerView: 3,
+            slidesPerGroup: 1,
+        },
+    },
+    scrollbar: {
+        el: ".swiper-scrollbar",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
